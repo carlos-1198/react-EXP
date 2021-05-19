5287fbfaa4c5d5d7b335dba3e7db76106c22e31c
carlos mendoza <cristianmathers1107@gmail.com>
2021-05-19 15:38:45 -0500

    adding BrowserRouter

--- src/index.js
+++ src/index.js
@@ -4,13 +4,9 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css'
 import './global.css';
 
-import BadgeNew from './pages/BadgeNew';
+import App from './components/App'
 
 const container = document.getElementById('app');
 ReactDOM.render(
-<BadgeNew
-firstName="Carlos" 
-lastName="Mendoza"
-email="kirit0@gmail.com"
-profession="Fontend Engineer"/>, container);
+<App/>, container);
 
